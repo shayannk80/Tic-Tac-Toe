e43ac5a9d1fb787f389ed385221306932fe64343
shayan <shayan.karimi1380.sk@gmail.com>
2024-08-09 21:11:52 +0330

    add player names

--- xo.js
+++ xo.js
@@ -1,62 +1,120 @@
-let div = document.getElementsByClassName("div");
-let c = 0, finish = true, a = ["","","","","","","","",""], s1, s2, ss = 1, k1 = 0, k2 = 0, k3 = 0;
-let go = document.getElementById("go");
-let clrs = document.getElementById("clrs");
-let start = document.getElementById("start");
+let cell = document.getElementsByClassName("cell");
+let choose = document.getElementById("color-selection");
+let colors = document.getElementById("color-options");
+let start = document.getElementById("start-menu");
 let final = document.getElementById("final");
-let finn = document.getElementById("finn");
+let rematch = document.getElementById("rematch-menu");
 let board = document.getElementById("board");
+let names = document.getElementById("name-selection");
+let playername = document.getElementById("playername");
+let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2");
+let gameboard = document.getElementById("game-board");
+let turnshow = document.getElementById("turn");
+
+let turn = 1, finish = true, a = ["","","","","","","","",""], color1, color2, symbol = 'x', score1 = 0, score2 = 0, match = 0, name1, name2, bothchoose = false;
 
 function play(){
-    board.style.display = "inline-flex";
-    start.style.display = "none";
-    go.style.display = "block";
-    clrs.style.display = "inline";
-    finn.style.display = "none";
-    c = k3, finish = true, a = ["","","","","","","","",""], ss = 1;
-    go.innerHTML = "X choose color";
-    reset();
+    if(match == 0){
+        start.style.display = "none";
+        names.style.display = "block";
+        board.style.display = "none";
+    }
+    else{
+        reset();        
+        rematch.style.display = "none";
+        choose.style.display = "block";
+        colors.style.display = "inline";
+        a = ["","","","","","","","",""];
+        bothchoose = false;
+    }
 }
+
 function reset(){
     for(let i = 0; i < 9; i++){
-        div[i].innerHTML = "-";
-        div[i].style.color = "white";
+        cell[i].innerHTML = "-";
+        cell[i].style.color = "white";
     }
 }
-function clr(s){
-    if(ss == 1){
-        go.innerHTML = "O choose color";
-        s1 = s;
-    }else{
-        s2 = s;
-        clrs.style.display = "none";
-        finish = false;
-        go.innerHTML = "playing now...";
+
+function submitNames(){
+    names.style.display = "none";
+    name1 = document.getElementById("name1").value;
+    name2 = document.getElementById("name2").value;
+    choose.style.display = "block";
+    colors.style.display = "inline";
+    playername.innerHTML = name1;
+    player1.innerHTML = name1;
+    player2.innerHTML = name2;
+}
+
+function selectColor(inputcolor){
+    if(turn == 1){
+        if(bothchoose == false){
+            playername.innerHTML = name2;
+            color1 = inputcolor;
+        }else{
+            color2 = inputcolor;
+            colors.style.display = "none";
+            finish = false;
+            gameboard.style.display = "block";
+            board.style.display = "inline-flex";
+            choose.style.display = "none";
+            turnshow.style.display = "inline";
+            document.getElementById("playerturn").innerHTML = name1;
+            turnshow.style.color = color1;
+            playername.innerHTML = name2;
+        }
     }
-   ss++;
+    else {
+        if(bothchoose == false){
+            playername.innerHTML = name1;
+            color2 = inputcolor;
+        }else{
+            color1 = inputcolor;
+            colors.style.display = "none";
+            finish = false;
+            gameboard.style.display = "block";
+            board.style.display = "inline-flex";
+            choose.style.display = "none";
+            turnshow.style.display = "inline";
+            document.getElementById("playerturn").innerHTML = name2;
+            turnshow.style.color = color2;
+            playername.innerHTML = name1;
+        }
+    }
+    bothchoose = true ;
 }
 
 for(let i = 0; i < 9; i++){
-    div[i].addEventListener("click", 
+    cell[i].addEventListener("click", 
         function(e){
-            let k;
+            let clickcell;
             for(let j = 0; j < 9; j++){
-                if(e.target == div[j]){
-                    k = j;
+                if(e.target == cell[j]){
+                    clickcell = j;
                 }
             }
-            if(!finish && a[k] == ""){
-                if(c%2 == 0){
-                    e.target.innerHTML = "x";
-                    e.target.style.color = s1;
-                    a[k] = "x";
+            if(!finish && a[clickcell] == ""){
+                e.target.innerHTML = symbol;
+                a[clickcell] = symbol;
+                if(turn == 1){
+                    e.target.style.color = color1;
+                    document.getElementById("playerturn").innerHTML = name2;
+                    turnshow.style.color = color2;
                 }
                 else{
-                    e.target.innerHTML = "o";
-                    e.target.style.color = s2;
-                    a[k] = "o";
+                    e.target.style.color = color2;
+                    document.getElementById("playerturn").innerHTML = name1;
+                    turnshow.style.color = color1;
                 }
-                c++;
+                if(symbol == 'x'){
+                    symbol = 'o';
+                }
+                else{
+                    symbol = 'x'
+                }
+                turn = (turn + 1) % 2;
                 checkfinish();
             }
          }
@@ -64,59 +122,53 @@ for(let i = 0; i < 9; i++){
 }
 
 function checkfinish(){
-    if((a[0]=='x' && a[1]=='x' && a[2]=='x') ||  (a[3]=='x' && a[4]=='x' && a[5]=='x') || (a[6]=='x' && a[7]=='x' && a[8]=='x')
-    || (a[0]=='x' && a[3]=='x' && a[6]=='x') || (a[1]=='x' && a[4]=='x' && a[7]=='x') || (a[2]=='x' && a[5]=='x' && a[8]=='x')
-    ||(a[0]=='x' && a[4]=='x' && a[8]=='x') || (a[2]=='x' && a[4]=='x' && a[6]=='x')){
-        alert("X win");
-        k1++;
-        k3++;
-        calculate();
-        go.style.display = "none";
-        finn.style.display = "inline";
+    if((a[0]==a[1] && a[2]==a[1] && a[0]!="") ||  (a[3]==a[4] && a[5]==a[3] && a[3]!="") || (a[6]==a[7] && a[8]==a[6] && a[6]!="")
+    || (a[0]==a[3] && a[6]==a[0] && a[0]!="") || (a[1]==a[4] && a[7]==a[1] && a[1]!="") || (a[2]==a[5] && a[8]==a[2] && a[2]!="")
+    ||(a[0]==a[4] && a[8]==a[0] && a[0]!="") || (a[2]==a[4] && a[6]==a[2] && a[2]!="")){
+        if(turn == 0){
+            score1++;
+            final.innerHTML = `${name1} Won`;
+            final.style.color = color1;
+        }   
+        else{
+            score2++;
+            final.innerHTML = `${name2} Won`;
+            final.style.color = color2;
+        }
+        match++;
+        document.getElementById("match").innerHTML = match;
+        document.getElementById("score1").innerHTML = score1;
+        document.getElementById("score2").innerHTML = score2;
+        gameboard.style.display = "none";
+        rematch.style.display = "inline";
+        turnshow.style.display = "none";
         finish = true;
+        turn = (match+1) % 2;
     }
-    if((a[0]=='o' && a[1]=='o' && a[2]=='o') ||  (a[3]=='o' && a[4]=='o' && a[5]=='o') || (a[6]=='o' && a[7]=='o' && a[8]=='o')
-    || (a[0]=='o' && a[3]=='o' && a[6]=='o') || (a[1]=='o' && a[4]=='o' && a[7]=='o') || (a[2]=='o' && a[5]=='o' && a[8]=='o')
-    ||(a[0]=='o' && a[4]=='o' && a[8]=='o') || (a[2]=='o' && a[4]=='o' && a[6]=='o')){
-        alert("O win");
-        k2++;
-        k3++;
-        calculate();
-        go.style.display = "none";
-        finn.style.display = "inline";
+    else if(a[0]!="" && a[1]!="" && a[2]!="" && a[3]!="" && a[4]!="" && a[5]!="" && a[6]!="" && a[7]!="" && a[8]!="" && !finish){
+        score1 += 0.5;
+        score2 += 0.5;
+        match++;
+        document.getElementById("match").innerHTML = match;
+        document.getElementById("score1").innerHTML = score1;
+        document.getElementById("score2").innerHTML = score2;
+        gameboard.style.display = "none";
+        rematch.style.display = "inline";
+        turnshow.style.display = "none";
         finish = true;
-    }
-    if(a[0]!="" && a[1]!="" && a[2]!="" && a[3]!="" && a[4]!="" && a[5]!="" && a[6]!="" && a[7]!="" && a[8]!="" && !finish){
-        alert("tie");
-        k3++;
-        k2 += 0.5;
-        k1 += 0.5;
-        calculate();
-        go.style.display = "none";
-        finn.style.display = "inline";
-        finish = true;
+        turn = (match+1) % 2;
+        final.innerHTML = `Draw`;
     }
 }
-function calculate(){
-    document.getElementById("k3").innerHTML = k3;
-    document.getElementById("k1").innerHTML = k1;
-    document.getElementById("k2").innerHTML = k2;
-}
+
 function end(){
-    go.style.display = "none";
-    finn.style.display = "none";
-    if(k1 > k2){
-        final.style.color = s1;
-        if(s1 == "black"){
-            final.style.color = "white";
-        } 
-        final.innerHTML= "Game Over <br> X is winner";
+    board.style.display = "none";
+    if(score1 > score2){
+        rematch.innerHTML = `Game Over <br> ${name1} <br> is winner`;
+        rematch.style.color = color1;
     }else if(k2 > k1){
-        final.style.color = s2;
-        if(s2 == "black"){
-            final.style.color = "white";
-        } 
-        final.innerHTML= "Game Over <br> O is winner";
+        rematch.innerHTML = `Game Over <br> ${name2} <br> is winner`;
+        rematch.style.color = color2;
     }else{
         final.innerHTML= "Game Over <br> nobody win";
     }
